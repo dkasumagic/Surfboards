cb711112c78027e6214783d4d5575e7ebffb8fa5
MiiKaa3 <m.kasumagic@gmail.com>
2025-08-17 01:33:10 +1000

    cleaned some things up

--- src/backend/verilog_writer.cpp
+++ src/backend/verilog_writer.cpp
@@ -1,9 +1,9 @@
-// Takes the matrixhandler output and produces valid verilog code.
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <filesystem>
 #include <cstdint>
+#include "verilog_writer.hpp"
 
 int writeVerilogFile(const std::string& inputFileName, const std::string& outputFileName, const size_t& numBits) {
     // HUMAN COMMENTS btw haha
